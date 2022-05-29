7ee7f9cdc048cf5ba359e1a90fa9ca7dd18a9f7f
Artiom_Sokolov <sokolova-net@mail.ru>
2022-05-29 22:51:04 +0300

    fix bugs my hooks

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,11 +1,11 @@
-import {useMemo} from "@types/react";
+import {useMemo} from "react";
 
 export const useSortedPosts = (posts, sort) => {
 
 
     const sortedPosts = useMemo(() => {
         if(sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[sort]));
+            return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]));
         }
         return posts;
     },[sort, posts])
