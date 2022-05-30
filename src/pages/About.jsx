426f7cf2c00cd4662061b900ca9b1e0cb8cc8f25
Artiom_Sokolov <sokolova-net@mail.ru>
2022-05-30 12:35:18 +0300

    addded routing on the page

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+           Это приложение создано в качеставе обучающего приложения
+        </h1>
+    );
+};
+
+export default About;
